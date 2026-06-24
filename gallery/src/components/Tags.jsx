d512978b8bca48d266713b638450a79e34b386f5
brew4242564 <dlvera.py@gmail.com>
2026-06-24 17:47:26 -0300

    Add tagwall component

---
+++ gallery/src/components/Tags.jsx
@@ -0,0 +1,24 @@
+import { getTags } from "../services/catsApi";
+import { useState, useEffect } from "react";
+
+export default function Tags({setTag}){
+    const [tags, setTags] = useState([]);
+    useEffect(()=> {
+        async function loadTags(){
+            const data = await getTags();
+            const cleanTags = data.filter(tag => tag && tag.length >0);
+            setTags(cleanTags)
+        }
+
+        loadTags();
+    },[])
+        
+
+    return(
+        <div className="tag-wall">
+            {tags.map((t)=> (
+                <button className="tag-btn" key={t} onClick={()=> setTag(t)}>{t}</button>
+            ))}
+        </div>
+    )
+}
