232f9df4e533da3ea66b309a69e23f24f61fb55e
brew4242564 <dlvera.py@gmail.com>
2026-07-14 15:09:22 -0300

    Add history on res.json

--- Gambler-API/index.js
+++ Gambler-API/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const App = express();
-const PORT = 3001;
+const PORT =  process.env.PORT || 3001;
 
 // BASE VALUES
 const BASE_MONEY = 100;
@@ -11,14 +11,14 @@ const BET_FREQUENCY_COMISSION = 10;
 // ESCENARIOS
 const BASE_CHANCES = { win: 0.5, lose: 0.5 };
 
-function betSimulation(money) {
+function betSimulation(money, bet_price) {
   const random = Math.random();
   if (random < BASE_CHANCES.win) {
-    const prize = money + 1;
-    return { win:true, balance: prize };
+    const prize = money + bet_price;
+    return { win: true, balance: prize };
   } else {
-    const prize = money - 1;
-    return { win:false, balance: prize };
+    const prize = Math.max(money - bet_price);
+    return { win: false, balance: prize };
   }
 }
 
@@ -27,12 +27,21 @@ App.get("/gambling", (req, res) => {
   let numberOfTry = 1;
   const history = [];
   for (numberOfTry; numberOfTry <= BASE_NUMBER_TRY; numberOfTry++) {
-    if (money <= 0) break;
-    numberOfTry--; //no se pudo jugar esta ronda, no deberia contarla.
-    money = betSimulation(money).balance;
-    if (numberOfTry % BET_FREQUENCY_COMISSION === 0) {
-      money = money - BASE_BET_COMISSION;
+    if (money < BASE_BET_PRICE) {
+      numberOfTry--; //no se pudo jugar esta ronda, no deberia contarla.
+      break;
     }
+
+    const { win, balance } = betSimulation(money, BASE_BET_PRICE);
+    money = balance;
+    if (numberOfTry % BET_FREQUENCY_COMISSION === 0) {
+      money = Math.max(0, money - BASE_BET_COMISSION);
+    }
+    history.push({
+      round: numberOfTry,
+      win: win,
+      currentMoney: money,
+    });
   }
 
   res.json({
@@ -40,6 +49,7 @@ App.get("/gambling", (req, res) => {
     BET_PRICE: BASE_BET_PRICE,
     TRY_SURVIVED: numberOfTry,
     FINAL_MONEY: money,
+    HISTORY: history,
   });
 });
 
